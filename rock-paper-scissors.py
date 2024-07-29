5ba86b4643b43a4660156bac8f10cfede4e15f56
Nachiket Mahajan <202301060047@mitaoe.ac.in>
2024-07-29 13:34:59 +0530

    Update rock-paper-scissors.py

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,36 +1,45 @@
 # python programming internship in codsoft 
 # task  -- rock-paper-scissors
+
 import random
 
 options = ['rock', 'paper', 'scissors']
-computer = random.choice(options)    
+
+
+
+computerchoice = random.choice(options)
 
 while True:
+
     ask = int(input("press 1 to play and 2 to exit:"))
     if ask == 1 :
-        print("options are ",options)
-        user= input("Enter your Choice :- ").lower()  
-        if user not in options :
-            print ("INVALID INPUT")
-        else:
-            print("Computer Choice :-" + computer)
-            if computer == user :
-                print("It's a  tie")
-            elif computer=="stone":
-                if user=="paper":
-                    print("User Wins !!")
-                else:
-                    print("computer Wins !!")
-            elif computer=="paper":
-                if user =="stone":
-                    print ("Computer Wins !!")
-                else :
-                    print ("User Wins !!")
-            elif computer=="scissors":
-                if user =="stone":
-                    print("User Wins !!")
-                else:
-                    print("Computer Wins !!")
+        userschoice = input('Enter your choice (rock, paper or scissors): ')
+        if userschoice == computerchoice:
+            print("Your choice ",userschoice, "computer choice" ,computerchoice,"\n")
+            print(" It's a tie!")
+        elif userschoice == "rock":
+            if computerchoice == "scissors":
+                print("Your choice ",userschoice, "computer choice" ,computerchoice,"\n")
+                print("You win! ")
+            else:
+                print("Your choice ",userschoice, "computer choice" ,computerchoice,"\n")
+                print("You lose!")
+        elif userschoice == "paper":
+            if computerchoice == "rock":
+                print("Your choice ",userschoice, "computer choice" ,computerchoice,"\n")
+                print("You win! ")
+            else:
+                print("Your choice ",userschoice, "computer choice" ,computerchoice,"\n")
+                print("You lose !")
+        elif userschoice == "scissors":
+            if computerchoice == "paper":
+                print("Your choice ",userschoice, "computer choice" ,computerchoice,"\n")
+                print("You win! ")
+            else:
+                print("Your choice ",userschoice, "computer choice" ,computerchoice,"\n")
+                print("You lose !")
+        else :
+             print("invalid")
     elif ask == 2:
         print("thank you for playing")
         break
